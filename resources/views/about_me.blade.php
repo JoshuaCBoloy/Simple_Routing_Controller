c51cb928599545782a768b95ec9b0a44e39d23ba
Jashqtieee <instajoshua04@gmail.com>
2024-02-17 18:16:23 +0800

    created a simple routing controller app in laravel with three buttons (about me, skills, hobbies)

---
+++ resources/views/about_me.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <a href="{{ url('/about-me') }}">About Me</a>
+  <a href="{{ url('/skills') }}">Skills</a>
+  <a href="{{ url('/hobbies') }}">Hobbies</a>
+
+  <h1>About Me</h1>
+  <p>Name:  Joshua C. Boloy</p>
+  <p>Age:  20</p>
+  <p>Gender:  Male</p>
+  <p>Hometown:  Baguio</p>
+  <p>Email: jbm5100@students.uc-bcf.edu.ph</p>
+</body>
+</html>
---
+++ resources/views/hobbies.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <a href="{{ url('/about-me') }}">About Me</a>
+  <a href="{{ url('/skills') }}">Skills</a>
+  <a href="{{ url('/hobbies') }}">Hobbies</a>
+
+  <h1>Hobbies</h1>
+  <p>Playing Basketball</p>
+  <p>Writing Poems</p>
+  <p>Listening to Music</p>
+</body>
+</html>
